10ec6f01b2c76f8140f218a1903be667f39326fa
Veselin Metodiev <veselin.metodiev@Veselins-Mac-mini.local>
2022-08-01 18:01:57 +0300

    Fixing everything in 03-My-Blogs

--- 03-My-Blogs-REST-ES6/js/blogs-api-client.js
+++ 03-My-Blogs-REST-ES6/js/blogs-api-client.js
@@ -1,14 +1,66 @@
 const API_BASE_URL = "http://localhost:3000/api/posts";
 
-//Fetch the posts from db.json. Returns promise
 export async function getAllPosts() {
-    try{
-    const postsResp = await fetch(API_BASE_URL);
-    if(postsResp.status >= 400) {
-        return Promise.reject(postsResp.body);
+    try {
+        const postsResp = await fetch(API_BASE_URL);
+        if(postsResp.status >= 400) {
+            return Promise.reject(postsResp.body);
+        }
+        return postsResp.json();
+    } catch(err) {
+        return Promise.reject(err);
     }
-    return postsResp.json();
-    } catch(err){
+}
+
+export async function addNewPost(post) {
+    try {
+        const postResp = await fetch(API_BASE_URL, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        });
+        if(postResp.status >= 400) {
+            return Promise.reject(postResp.body);
+        }
+        return postResp.json();
+    } catch(err) {
+        return Promise.reject(err);
+    }
+}
+
+export async function editPostId(post, postId) {
+    try {
+        const postResp = await fetch(API_BASE_URL + "/" + postId, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        });
+        if(postResp.status >= 400) {
+            return Promise.reject(postResp.body);
+        }
+        return postResp.json();
+    } catch(err) {
+        return Promise.reject(err);
+    }
+}
+
+export async function deletePostId(postID) {
+    try {
+        const postResp = await fetch(API_BASE_URL + "/" + postID, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+        if(postResp.status >= 400) {
+            return Promise.reject(postResp.body);
+        }
+        return postResp.json();
+    } catch(err) {
         return Promise.reject(err);
     }
 }
--- 03-My-Blogs-REST-ES6/js/index.js
+++ 03-My-Blogs-REST-ES6/js/index.js
@@ -1,58 +1,117 @@
-import { getAllPosts } from "./blogs-api-client.js";
+import { addNewPost, getAllPosts, deletePostId, editPostId } from './blogs-api-client.js';
+import { chipsInstances } from './materialize-helpers.js';
 
-//get elements from index.html
 const postsSection = document.getElementById("posts");
-const errorsDiv = document.getElementById("errors");
+const erorrsDiv = document.getElementById("errors");
+const addPostForm = document.getElementById("add-post-form");
+addPostForm.addEventListener('submit', handleSubmitPost);
+addPostForm.addEventListener('reset', resetForm);
 
-//gets and shows posts
-async function init(){
-    try {
+async function init() {
+  try {
     const allPosts = await getAllPosts();
     showPosts(allPosts);
-} catch(err) {
+  } catch (err) {
     showError(err);
-}
-}
-
-//Iterate through the posts
-export function showPosts(posts){
-posts.forEach(post => addPost(post));
+  }
 }
 
-//Separate function for showing an error
-export function showError(err){
-    errorsDiv.innerHTML = `<div>${err}</div>`;
+export function showPosts(posts) {
+  posts.forEach(post => addPost(post));
 }
 
-//create the html element to add posts to the index.html
-export function addPost(post){
-    const postElem = document.createElement('article');
-    postElem.className = "col s12 m6 l4";
-    // postElem.innerHTML = `
-    // <h3 class="post-title">${post.title}</h3>
-    // <img class="post-img" src=${post.imgUrl}>
-    // <div class="post-content">
-    //     <div class="post-metadata"> Author: ${post.authorId}, Tags: ${post.tags ? post.tags.join(', ') : 'no tags'}</div>
-    //     <div class="post-text"> ${post.content}</div>
-    // </div>
-    //     `
+export function showError(err) {
+  erorrsDiv.innerHTML = `<div>${err}</div>`;
+}
 
-        postElem.innerHTML = `
-        <div class="card">
+export async function deletePost(postId) {
+    await deletePostId(postId);
+    const postElem = document.getElementById('posts');
+    postElem.innerHTML = '';
+    init();
+  }
+
+  export async function editPost(event, postId) {
+    console.log(postId);
+    try {
+        event.preventDefault();
+        const formData = new FormData(addPostForm);
+        const newPost = {};
+        for (const entry of formData.entries()) {
+          newPost[entry[0]] = entry[1];
+        }
+        const tags = chipsInstances[0].chipsData.map(chips => chips.tag);
+        newPost['tags'] = tags;
+        await editPostId(newPost, postId);
+        const postElem = document.getElementById('posts');
+        postElem.innerHTML = '';
+        init();
+        // await deletePost(postId);
+        // const created = await addNewPost(newPost);
+        // addPost(created);
+        resetForm();
+      } catch (err) {
+        showError(err);
+      }
+  }
+
+export function addPost(post) {
+  const postElem = document.createElement('article');
+  postElem.setAttribute('id', post.id);
+  postElem.className = "col s12 m6 l4";
+  postElem.innerHTML = `
+    <div class="card">
     <div class="card-image waves-effect waves-block waves-light">
       <img class="activator" src="${post.imgUrl}">
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4">${post.title}<i class="material-icons right">more_vert</i></span>
-      <div class="post-metadata"> Author: ${post.authorId}, Tags: ${post.tags ? post.tags.join(', ') : 'no tags'}</div>
+      <p>Author: ${post.authorId}, Tags: ${post.tags ? post.tags.join(', ') : 'no tags'}</p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${post.title}<i class="material-icons right">close</i></span>
       <p>${post.content}</p>
-  </div>
-  </div>
-  `
-    postsSection.insertAdjacentElement('beforeend', postElem)
+    </div>
+    <div class="card-action">
+      <button class="btn waves-effect waves-light" type="button" id="edit">Edit
+        <i class="material-icons right">send</i>
+      </button>
+      <button class="btn waves-effect waves-light red lighten-1" type="button" id="delete">Delete
+        <i class="material-icons right">clear</i>
+      </button>
+    </div>
+    </div>
+    `;
+  postsSection.insertAdjacentElement("beforeend", postElem);
+  postElem.querySelector('#delete').addEventListener('click', event => deletePost(post.id))
+  postElem.querySelector('#edit').addEventListener('click', event => editPost(event, post.id));
 }
 
-init();
+async function handleSubmitPost(event) {
+  try {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newPost = {};
+    for (const entry of formData.entries()) {
+      newPost[entry[0]] = entry[1];
+    }
+    const tags = chipsInstances[0].chipsData.map(chips => chips.tag);
+    newPost['tags'] = tags;
+    const created = await addNewPost(newPost);
+    addPost(created);
+    resetForm();
+  } catch (err) {
+    showError(err);
+  }
+}
+
+export function resetForm() {
+  addPostForm.reset();
+  const instance = chipsInstances[0];
+  while(instance.chipsData.length > 0) {
+    instance.deleteChip(0);
+  }
+}
+
+
+init()
