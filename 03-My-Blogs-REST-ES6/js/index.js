eac6e3a5a3298cd7bf17d9e5a2d9010189e79273
Veselin Metodiev <veselin.metodiev@Veselins-Mac-mini.local>
2022-08-01 12:16:47 +0300

    change postion of card

--- 03-My-Blogs-REST-ES6/js/index.js
+++ 03-My-Blogs-REST-ES6/js/index.js
@@ -27,7 +27,7 @@ export function showError(err){
 //create the html element to add posts to the index.html
 export function addPost(post){
     const postElem = document.createElement('article');
-    postElem.className = "col s12 m4";
+    postElem.className = "card col s12 m4";
     // postElem.innerHTML = `
     // <h3 class="post-title">${post.title}</h3>
     // <img class="post-img" src=${post.imgUrl}>
@@ -38,7 +38,6 @@ export function addPost(post){
     //     `
 
         postElem.innerHTML = `
-        <div class="card">
     <div class="card-image waves-effect waves-block waves-light">
       <img class="activator" src="${post.imgUrl}">
     </div>
@@ -49,7 +48,6 @@ export function addPost(post){
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${post.title}<i class="material-icons right">close</i></span>
       <p>${post.content}</p>
-    </div>
   </div>`
     postsSection.insertAdjacentElement('beforeend', postElem)
 }
