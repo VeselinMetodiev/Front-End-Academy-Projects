8213da4a785a5618f95a175abe0ab2bc9c6444f1
Veselin Metodiev <veselin.metodiev@Veselins-Mac-mini.local>
2022-08-02 16:53:22 +0300

    typescript introduction

--- 05 Typescript intro/src/greeter.ts
+++ 05 Typescript intro/src/greeter.ts
@@ -1,9 +1,62 @@
-function greeter(name: string): string{
-    return `Hello ${name} from Typescript`;
+function greeter(name = 'Anonimous', date = new Date()): string{
+    return `Hello ${name} from Typescript on ${date?.toDateString()}`;
 }
 
-// document.getElementById('results')!.innerHTML = greeter("Veselin");
-const elem = document.getElementById('results');
-if(elem != null) {
-    elem.innerHTML = greeter("Georgi");
+document.getElementById('results')!.innerHTML = greeter("Veselin", new Date('1995-12-17T03:24:00'));
+document.getElementById('results')!.innerHTML = greeter();
+// const elem = document.getElementById('results');
+// if(elem !== null) {
+//     elem.innerHTML = greeter("Georgi");
+// }
+
+function printId(id: number | string){
+    //type guarding
+    if(typeof id === "string"){
+    console.log(id.toUpperCase());
+    } else {
+        console.log(++id);
+    }
 }
+
+
+function welcomePeople(x: string[] | string) {
+    if (Array.isArray(x)) {
+      // Here: 'x' is 'string[]'
+      console.log("Hello, " + x.join(" and "));
+    } else {
+      // Here: 'x' is 'string'
+      console.log("Welcome lone traveler " + x);
+    }
+  }
+
+  //instanceof guard narrowing
+  function logValue(x: Date | string) {
+    if (x instanceof Date) {
+      console.log(x.toUTCString());               
+    } else {
+      console.log(x.toUpperCase());           
+    }
+  }
+
+  logValue(new Date('1995-12-17T03:24:00'));
+  logValue("Veselin");
+
+  welcomePeople(["Dimitar", "Ivan", "Hristo"]);
+  welcomePeople("John Smith");
+
+printId(42);
+printId("abracadabra");
+
+//literal types
+type Point = {
+    x: number;
+    y: number;
+  };
+   
+  // Exactly the same as the earlier example
+  function printCoord(pt: Point) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+  }
+   
+  printCoord({ x: 100, y: 100 });
