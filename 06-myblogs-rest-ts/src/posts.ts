43e7c024d36c8ae56a8620984de49abd1ae93e36
Veselin Metodiev <veselin.metodiev@Veselins-Mac-mini.local>
2022-08-05 11:28:22 +0300

    postRepository Complete

--- 06-myblogs-rest-ts/src/posts.ts
+++ 06-myblogs-rest-ts/src/posts.ts
@@ -1,3 +1,4 @@
+import { Repository, RepositoryInMemory } from "./repository.js";
 import { IdType } from "./shared-types.js";
 
 export class PostCreateDto {
@@ -22,4 +23,54 @@ export class Post extends PostCreateDto {
         ) {
             super(title, content, tags, imageUrl, authorId)
     }
+}
+
+export interface PostRepository extends Repository<IdType, Post>{
+    findByTags(searchTags: string[]) : Post[];
+    findByTitlePart(titlePart: string) : Post[];
+    findByAuthorId(authorId: IdType) : Post[];
+}
+
+export class PostRepositoryImpl extends RepositoryInMemory<IdType, Post> implements PostRepository {
+    findByTags(searchTags: string[]): Post[] {
+        return this.findAll().filter(post => post.tags.some(tag => searchTags.includes(tag)));
+    }
+    findByTitlePart(titlePart: string): Post[] {
+        return this.findAll().filter(post => post.title.includes(titlePart));
+    }
+    findByAuthorId(authorId: number): Post[] {
+        return this.findAll().filter(post => post.authorId === authorId);
+    }
+}
+
+interface IdGenerator<K>{
+    getNextId(): K;
+}
+
+export class NumberIdGenerator implements IdGenerator<number> {
+    private nextId = 0;
+    getNextId(): number {
+        return ++this.nextId;
+    }
+
+}
+
+const SAMPLE_POST = [
+    new Post(0, "New in Typescript", "Typescript becomes stricter...", ['typescript', 'novelties'], "https://assets.logitech.com/assets/64683/performance-mouse-mx.png", 1),
+    new Post(0, "New in ECMAScript", "ES becomes stricter...", ['ecmascript', 'novelties'], "https://assets.logitech.com/assets/64683/performance-mouse-mx.png", 1),
+    new Post(0, "New in JavaScript", "Typescript becomes stricter...", ['javascript', 'novelties'], "https://assets.logitech.com/assets/64683/performance-mouse-mx.png", 2),
+];
+
+function testPostRepository() {
+    const postRepo: PostRepository = new PostRepositoryImpl(new NumberIdGenerator());
+    SAMPLE_POST.forEach(post => postRepo.create(post));
+    console.log('Find all');
+    
+    postRepo.findAll().forEach(post => console.log(post));
+    console.log('Find by Tags');
+    postRepo.findByTags(['novelties', 'es']).forEach(post => console.log(post));
+    console.log('Find by title');
+    postRepo.findByTitlePart('Script').forEach(post => console.log(post));
+    console.log('Find by author');
+    postRepo.findByAuthorId(2).forEach(post => console.log(post));
 }
